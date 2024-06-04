340840878e7373b80124c0d3ce9b607a8e6f64e0
Filippo Dalla Chiara <filippo.dallachiara@mail-bip.com>
2024-06-04 20:05:13 +0200

    fixed job

--- job.sh
+++ job.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=matrixMultiplication
+#SBATCH --nodes=1
 #SBATCH --time=01:00
 #SBATCH --ntasks=2
-#SBATCH --nodes=1
-#SBATCH -o output.log # redirect STDOUT
-#SBATCH -e errors.log # redirect STDERR
+#SBATCH -o output.log # assign STDOUT
+#SBATCH -e errors.log # assign STDERR
 
 module load singularity
-
 export TMPDIR=$HOME/tmp
 mkdir -p $TMPDIR
 
-singularity exec --bind $TMPDIR:$TMPDIR  matrix-multiplication.sif bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 /prj/main"
+singularity exec --bind $TMPDIR:$TMPDIR  matrix-multiplication.sif bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 main"
